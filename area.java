29989a8c41b9fb21760e5b0fcfe134ad8f0ca91f
AbhaySingh713 <abhaysinghbhadauriya713@gmail.com>
2025-02-02 19:53:54 +0530

    third commit

---
+++ area.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class area{
+    area(double r)
+    {
+        System.out.print(  Math.PI *r*r);
+    }
+    public static void main(String args [])
+    {
+        area obj = new area(2.43);
+        System.out.println("hello");
+
+    }
+}
